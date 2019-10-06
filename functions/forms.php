f1e453e3199f3b95cb31ec0657f6a0941600c926
Jackson <hello@jacksonlewis.co.uk>
2019-10-06 19:19:15 +0100

    Set up sample of shiftr_register_form() as a comment

--- functions/forms.php
+++ functions/forms.php
@@ -5,66 +5,42 @@
     */
 
 
-	// The General Form
-	shiftr_register_form(
-		'general',
-		array(
-			'settings' => array(
-				'nicename' 	=> 'General',
-				'labels' => false
-			),
-			'fields' => array(
-				array(
-					'type' 		=> 'text',
-					'name'		=> 'name'
-				),
-				array(
-					'type' 		=> 'email',
-					'name'		=> 'email'
-				),
-				array(
-					'type'		=> 'select',
-					'name'		=> 'enquiry_type',
-					'required'  => false,
-					'options'   => array(
-						'value1' => 'Value 1',
-						'value2' => 'Value 2'
-					)
-				),
-				array(
-					'type'		=> 'radio',
-					'name'		=> 'kitchen-type',
-					'radio_fields' => array(
-						array(
-							'name' => 'black'
-						),
-						array(
-							'name' => 'green'
-						),
-						array(
-							'name' => 'blue'
-						)
-					)
-				),
-				array(
-					'type' 		=> 'textarea',
-					'name'		=> 'message',
-					'rows'		=> 6
-				),
-				array(
-					'type' 		=> 'checkbox',
-					'name'		=> 'accept',
-					'label' 	=> 'I agree to the Terms & Conditions.',
-					'include_in_send' => false
-				)
-			)
-		)
-	);
+	// Sample of using shiftr_register_form()
+	// shiftr_register_form(
+	// 	'general',
+	// 	array(
+	// 		'settings' => array(
+	// 			'nicename' 	=> 'General',
+	// 			'labels' => false
+	// 		),
+	// 		'fields' => array(
+	// 			array(
+	// 				'type' 		=> 'text',
+	// 				'name'		=> 'name'
+	// 			),
+	// 			array(
+	// 				'type' 		=> 'email',
+	// 				'name'		=> 'email'
+	// 			),
+	// 			array(
+	// 				'type' 		=> 'textarea',
+	// 				'name'		=> 'message',
+	// 				'rows'		=> 6
+	// 			),
+	// 			array(
+	// 				'type' 		=> 'checkbox',
+	// 				'name'		=> 'accept',
+	// 				'label' 	=> 'I agree to the Terms & Conditions.',
+	// 				'include_in_send' => false
+	// 			)
+	// 		)
+	// 	)
+	// );
 
 
-	// The callable function to output the form HTML
-	function shiftr_form_general() {
+	// Sample of creating a function to call a forms HTML to be output
+	// function shiftr_form_general() {
 
-		shiftr_build_form( 'general' );
-	}
+	// 	shiftr_build_form( 'general' );
+	// }
 
