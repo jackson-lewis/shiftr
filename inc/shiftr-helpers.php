e66edd4fc98dd104c15afd2323a936d0077a7662
Jackson <hello@jacksonlewis.co.uk>
2019-11-07 13:44:36 +0000

    Updated shiftr_featured_image() to use core WP get_the_post_thumbnail()

--- inc/shiftr-helpers.php
+++ inc/shiftr-helpers.php
@@ -4,80 +4,41 @@
 /**  
  *  shiftr_featured_image
  *
- *  Output the current post featured image in multiple formats, default as <img>
+ *  Output the html of the current post featured image
  *
  *  @since 1.0
  *
- *	@global $post Get post ID and featured image of the post
- *	@param $args array The arguments passed
- *  @return mixed Depending on requested format as per $type
+ *	@param $id null|int The post ID
+ *  @param $size str The size of the image to output
+ *	@param $lazy bool Set if the image should be lazy loaded
+ *	@param $attr array Attributes that should be added to the img tag
  */
 
-function shiftr_featured_image( $args = [] ) {
+function shiftr_featured_image( $id = null, $size = 'large', $lazy = true, $attr = [] ) {
 
-	global $post;
+	$html = '';
 
-	$defaults = array(
-		'post_id' => null,
-		'type' => 'img',
-		'atts' => [],
-		'lazy' => false
+	$html = get_the_post_thumbnail( $id, $size, $attr );
+
+	$patterns = array(
+		'/width=\"[0-9]*\"/',
+		'/height=\"[0-9]*\"/',
+		'/class=\"[a-zA-Z0-9\s\-_]*\"/'
 	);
 
-	$args = (object) wp_parse_args( $args, $defaults );
+	$html = preg_replace( $patterns, '', $html );
 
+	if ( $lazy ) {
+		$patterns = array(
+			'/ (src=)/',
+			'/ (srcset=)/'
+		);
 
-	if ( $args->post_id === null ) {
-		$args->post_id = $post->ID;
+		$html = preg_replace( $patterns, ' data-\1', $html );
+		$html = preg_replace( '/(<img\s)/', '\1 class="lazy" ', $html );
 	}
 
-	// Assign attachment id if featured image is set
-	$image_id = get_post_thumbnail_id( $args->post_id );
-
-	// If featured image is not set, use placeholder
-	$image_id = ( $image == '' ) ? get_field( 'thumbnail_placeholder', 'option' ) : $image_id;
-
-	// Get image URL
-	$image_url = wp_get_attachment_url( $image_id );
-
-	$atts = $args->atts;
-
-
-	// Handle the different image types
-
-	if ( $args->type == 'img' ) {
-
-		if ( $args->lazy ) {
-			$atts['data-src'] = $image_url;
-
-			if ( isset( $atts['class'] ) ) {
-				$atts['class'] .= ' lazy';
-			} else {
-				$atts['class'] = 'lazy';
-			}
-
-			$atts['loading'] = 'lazy';
-
-		} else {
-			$atts['src'] = $image_url;
-		}
-
-		$atts['alt'] = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
-
-		// Apply any filters
-		$atts = apply_filters( 'shiftr_featured_image_atts', $atts );
-
-		echo '<img ' . shiftr_output_attr( $atts ) . '>';
-
-	} elseif ( $args->type == 'background' ) {
-		$atts['style'] = 'background-image: url(' . $image_url . ');';
-
-		echo shiftr_output_attr( $atts );
-
-	} elseif ( $args->type == 'raw' ) {
-		echo $image_url;
-
-	} else { return false; }
+	echo $html;
 }
 
 
