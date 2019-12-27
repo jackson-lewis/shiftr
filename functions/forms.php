5301e84a50b4dd11a258e69454b4d79d79b1be3c
Jackson <hello@jacksonlewis.co.uk>
2019-12-27 23:37:02 +0000

    Updated default imagery assets

--- functions/forms.php
+++ functions/forms.php
@@ -6,41 +6,46 @@
 
 
 	// Sample of using shiftr_register_form()
-	// shiftr_register_form(
-	// 	'general',
-	// 	array(
-	// 		'settings' => array(
-	// 			'nicename' 	=> 'General',
-	// 			'labels' => false
-	// 		),
-	// 		'fields' => array(
-	// 			array(
-	// 				'type' 		=> 'text',
-	// 				'name'		=> 'name'
-	// 			),
-	// 			array(
-	// 				'type' 		=> 'email',
-	// 				'name'		=> 'email'
-	// 			),
-	// 			array(
-	// 				'type' 		=> 'textarea',
-	// 				'name'		=> 'message',
-	// 				'rows'		=> 6
-	// 			),
-	// 			array(
-	// 				'type' 		=> 'checkbox',
-	// 				'name'		=> 'accept',
-	// 				'label' 	=> 'I agree to the Terms & Conditions.',
-	// 				'include_in_send' => false
-	// 			)
-	// 		)
-	// 	)
-	// );
+	shiftr_register_form(
+		'general',
+		array(
+			'settings' => array(
+				'nicename' 	=> 'General',
+				'labels' => false
+			),
+			'fields' => array(
+				array(
+					'type' 		=> 'text',
+					'name'		=> 'name'
+				),
+				array(
+					'type' 		=> 'email',
+					'name'		=> 'email'
+				),
+				array(
+					'type' 		=> 'tel',
+					'name'		=> 'phone',
+					'required'  => false
+				),
+				array(
+					'type' 		=> 'textarea',
+					'name'		=> 'message',
+					'rows'		=> 6
+				),
+				array(
+					'type' 		=> 'checkbox',
+					'name'		=> 'accept',
+					'label' 	=> 'I agree to the Terms & Conditions.',
+					'include_in_send' => false
+				)
+			)
+		)
+	);
 
 
 	// Sample of creating a function to call a forms HTML to be output
-	// function shiftr_form_general() {
+	function shiftr_form_general() {
 
-	// 	shiftr_build_form( 'general' );
-	// }
+		shiftr_build_form( 'general' );
+	}
 
