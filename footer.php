7c0f430faefc20faad1419ad8bec19eb16ac0f65
Jackson <hello@jacksonlewis.co.uk>
2019-12-27 23:17:12 +0000

    Footer is more of blank canvas

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 		<footer class="site-footer">
 			<div class="container">
 				
-				<div class="footer-info">
-					<small class="creator">Built by <a href="https://jacksonlewis.co.uk" target="_blank" rel="noopener">jacksonlewis.co.uk</a></small>
-				</div>
+				
 				
 			</div>
 		</footer>
