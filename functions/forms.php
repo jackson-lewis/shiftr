d248cee2401b6d3d23f64a7bbf2370b79c04ac5b
Jackson <hello@jacksonlewis.co.uk>
2021-03-02 09:18:27 +0000

    Reset form

--- functions/forms.php
+++ functions/forms.php
@@ -6,47 +6,47 @@
 
 
     // Sample of using shiftr_register_form()
-    shiftr_register_form(
-     'general',
-     array(
-         'settings' => array(
-             'nicename'  => 'General',
-             'labels' => false
-         ),
-         'fields' => array(
-             array(
-                 'type'      => 'text',
-                 'name'      => 'name'
-             ),
-             array(
-                 'type'      => 'email',
-                 'name'      => 'email'
-             ),
-             array(
-                 'type'      => 'file',
-                 'name'      => 'cv',
-                 'required'  => false,
-                 'file_types' => 'pdf,docx'
-             ),
-             array(
-                 'type'      => 'textarea',
-                 'name'      => 'message',
-                 'rows'      => 6
-             ),
-             array(
-                 'type'      => 'checkbox',
-                 'name'      => 'accept',
-                 'label'     => 'I agree to the Terms & Conditions.',
-                 'include_in_send' => false
-             )
-         )
-     )
-    );
+    // shiftr_register_form(
+    //  'general',
+    //  array(
+    //      'settings' => array(
+    //          'nicename'  => 'General',
+    //          'labels' => false
+    //      ),
+    //      'fields' => array(
+    //          array(
+    //              'type'      => 'text',
+    //              'name'      => 'name'
+    //          ),
+    //          array(
+    //              'type'      => 'email',
+    //              'name'      => 'email'
+    //          ),
+    //          array(
+    //              'type'      => 'file',
+    //              'name'      => 'cv',
+    //              'required'  => false,
+    //              'file_types' => 'pdf,docx'
+    //          ),
+    //          array(
+    //              'type'      => 'textarea',
+    //              'name'      => 'message',
+    //              'rows'      => 6
+    //          ),
+    //          array(
+    //              'type'      => 'checkbox',
+    //              'name'      => 'accept',
+    //              'label'     => 'I agree to the Terms & Conditions.',
+    //              'include_in_send' => false
+    //          )
+    //      )
+    //  )
+    // );
 
 
     // Sample of creating a function to call a forms HTML to be output
-    function shiftr_form_general() {
+    // function shiftr_form_general() {
 
-     shiftr_build_form( 'general' );
-    }
+    //  shiftr_build_form( 'general' );
+    // }
 
