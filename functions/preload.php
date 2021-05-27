ad1e6103d800e3410682edba81dd601a0f770f59
Jackson <hello@jacksonlewis.co.uk>
2021-05-27 16:25:24 +0100

    Setup of filters for preload

--- functions/preload.php
+++ functions/preload.php
@@ -6,22 +6,22 @@ function shiftr_preload_post_thumbnail() {
     global $post;
     /**
      * Prevent preloading for specific content types or post types
+     * 
+     * Defaults to true becuase preload must be on an opt-in basis.
      */
-    if ( ! is_singular() || is_singular( array( 'page' ) ) ) {
+    if ( apply_filters( 'shiftr_preload_post_thumbnail_early_exit', true, $post ) ) {
         return;
     }
-
-    $image_size = 'full';
     /**
-     * Adjust image size based on post type
+     * Set the size of the image
      */
-    if ( is_singular( 'post' ) ) {
-        $image_size = 'large';
-    }
-
-    $thumbnail_id = apply_filters( 'shiftr_featured_image_thumbnail_id', get_post_thumbnail_id( $post->ID ) );
+    $image_size = apply_filters( 'shiftr_preload_post_thumbnail_size', 'large', get_post_thumbnail_id( $post->ID ) );
     /**
-     * Get the image
+     * Set the attachment ID that should be used
+     */
+    $thumbnail_id = apply_filters( 'shiftr_preload_post_thumbnail_id', get_post_thumbnail_id( $post->ID ) );
+    /**
+     * Get the image, we should have everything we need now.
      */
     $image = wp_get_attachment_image_src( $thumbnail_id, $image_size );
     $src = '';
@@ -68,4 +68,51 @@ function shiftr_preload_post_thumbnail() {
      */
     printf( '<link rel="preload" as="image" href="%s" %s/>', esc_url( $src ), $additional_attr );
 }
-add_action( 'wp_head', 'shiftr_preload_post_thumbnail' );
+add_action( 'wp_head', 'shiftr_preload_post_thumbnail', 1 );
+
+
+/**
+ * Return true if the function should not run.
+ * 
+ * @param bool $exit Defaults to true
+ */
+add_filter( 'shiftr_preload_post_thumbnail_early_exit', function( $exit ) {
+
+    if ( is_singular( array( 'post' ) ) ) {
+        return false;
+    }
+
+    if ( is_singular( array( 'page' ) ) && get_page_template_slug() == 'templates/flexi-blocks-builder.php' ) {
+        return false;
+    }
+
+    return true;
+});
+
+/**
+ * Filter the size of the image that should be preloaded.
+ * 
+ * @param string $size `large` by default
+ */
+add_filter( 'shiftr_preload_post_thumbnail_size', function( $size ) {
+
+    if ( is_singular( 'post' ) ) {
+        $size = 'large';
+    }
+
+    if ( is_singular( array( 'page' ) ) && get_page_template_slug() == 'templates/flexi-blocks-builder.php' ) {
+        $size = 'full';
+    }
+
+    return $size;
+});
+
+/**
+ * Filter the size of the image that should be preloaded.
+ * 
+ * @param integer $id Post thumbnail ID
+ */
+add_filter( 'shiftr_preload_post_thumbnail_id', function( $id ) {
+
+    return $id;
+});
