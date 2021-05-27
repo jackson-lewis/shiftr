f70cd2d34102acc6d5841e3aa87c244fcf558431
Jackson <hello@jacksonlewis.co.uk>
2021-05-27 16:25:42 +0100

    Critical resources order and font loading strategy

--- header.php
+++ header.php
@@ -14,13 +14,13 @@
         
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <?php wp_head(); ?>
+
         <meta name="theme-color" content="<?php $shiftr->the( 'primary_color' ); ?>"> 
-        
         <link rel="icon" type="image/png" href="<?php shiftr_get_asset_url( 'sr@32.png', '_shiftr' ); ?>" sizes="32x32">
         <link rel="apple-touch-icon" href="<?php shiftr_get_asset_url( 'sr@128.png', '_shiftr' ); ?>" />
         <script>document.documentElement.classList.add('js');</script>
-
-        <?php wp_head(); ?>
     </head>
     
     <body <?php shiftr_body_class(); ?>>
