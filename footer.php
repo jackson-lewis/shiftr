41926cf4be39ae46a3cea292baf3baf3633afa47
Jackson <hello@jacksonlewis.co.uk>
2021-06-04 15:46:17 +0100

    New default site footer🎉

--- footer.php
+++ footer.php
@@ -1,8 +1,32 @@
         <footer class="site-footer">
-            <div class="container">
-                
-                
-                
+            <div class="footer-row--main">
+                <div class="container">
+                    <div class="footer-col">
+                        <?php shiftr_inline_svg( 'shiftr-full', '/assets/_shiftr/' ); ?>
+                        <address class="contact-details">
+                            <?php
+
+                            echo do_shortcode( '[phone_link]' );
+                            echo do_shortcode( '[email_link]' );
+                            printf( '<p>%s</p>', shiftr()->address );
+
+                            ?>
+                        </address>
+                    </div>
+                    <div class="footer-col">
+                        <span class="footer-heading"><?php echo wp_get_nav_menu_name( 'footer-1' ); ?></span>
+                        <?php shiftr_nav_footer_1(); ?>
+                    </div>
+                    <div class="footer-col">
+                        <span class="footer-heading"><?php echo wp_get_nav_menu_name( 'footer-2' ); ?></span>
+                        <?php shiftr_nav_footer_2(); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-row--legal">
+                <div class="container">
+                    <small>&copy; <?php echo date( 'Y' ); ?></small>
+                </div>
             </div>
         </footer>
 
