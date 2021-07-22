2c2c9e8259858a4963c80abf04023d5a795a7dbc
Jackson <hello@jacksonlewis.co.uk>
2021-07-22 17:12:36 +0100

    Clean up of header

--- header.php
+++ header.php
@@ -7,10 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <?php wp_head(); ?>
-
-        <meta name="theme-color" content="<?php $shiftr->the( 'primary_color' ); ?>"> 
-        <link rel="icon" type="image/png" href="<?php shiftr_get_asset_url( 'sr@32.png', '_shiftr' ); ?>" sizes="32x32">
-        <link rel="apple-touch-icon" href="<?php shiftr_get_asset_url( 'sr@128.png', '_shiftr' ); ?>" />
+ 
         <script>document.documentElement.classList.add('js');</script>
     </head>
     
