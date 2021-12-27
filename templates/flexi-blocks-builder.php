70f082bc11504a96a83b60b2eef7efa385fd7196
Jackson Lewis <hello@jacksonlewis.co.uk>
2021-12-27 22:22:48 +0000

    Revised standard hero banner

--- templates/flexi-blocks-builder.php
+++ templates/flexi-blocks-builder.php
@@ -13,28 +13,29 @@ get_header();
 
 ?>
 
+<main class="site-main">
+    <div class="hero--standard<?php echo has_post_thumbnail() ? ' with-post-thumbnail' : ''; ?>">
+        <?php if ( has_post_thumbnail() ) : ?>
+        <div class="hero-feature-image">
+            <?php shiftr_featured_image( get_the_ID(), 'full', array( 'loading' => false ) ); ?>
+        </div>
+        <?php endif; ?>
+        <div class="hero-content">
+            <div class="container">
+                <?php 
+                    if ( have_posts() ) {
+                        while ( have_posts() ) {
+                            the_post();
 
-<div class="hero--standard">
-    <div class="hero-feature-image">
-        <?php shiftr_featured_image( get_the_ID(), 'full', array( 'loading' => false ) ); ?>
-    </div>
-    <div class="hero-content">
-        <div class="container">
-            <?php 
-                if ( have_posts() ) {
-                    while ( have_posts() ) {
-                        the_post();
-
-                        the_content();
+                            the_title( '<h1>', '</h1>' );
+                            the_content();
+                        }
                     }
-                }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
-</div>
 
-
-<main class="site-main">
     <?php shiftr_flexi_blocks_builder( 'main' ) ?>
 </main>
     
