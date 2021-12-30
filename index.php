6f90831dd0fef83ec4cfb77dae5ac9802db53513
Jackson Lewis <hello@jacksonlewis.co.uk>
2021-12-30 16:24:06 +0000

    New template format for archive

--- index.php
+++ index.php
@@ -2,39 +2,4 @@
 /**
  * Index
  */
-get_header();
-
-?>
-
-<main class="site-main">
-    <div class="hero--basic">
-        <header class="container hero-content">
-            <h1><?php single_post_title(); ?></h1>
-        </header>
-    </div>
-
-    <section class="site-section posts-loop">
-        <div class="container">
-            
-            <?php if ( have_posts() ) : ?>
-                <ul class="posts loop">
-                    <?php
-                        while( have_posts() ) {
-                            the_post();
-
-                            shiftr_get_template( 'content-post.php' );
-                        }
-
-                        wp_reset_postdata();
-                    ?>
-                </ul>
-
-                <?php shiftr_pagination(); ?>
-            <?php endif; ?>
-
-        </div>
-    </section>
-</main>
-    
-
-<?php get_footer(); ?>
+shiftr_get_template( 'archive-post.php' );
---
+++ parts/archive-post.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * Default archive template.
+ */
+get_header();
+
+?>
+
+<main class="site-main">
+    <div class="hero--basic">
+        <header class="container hero-content">
+            <h1>
+                <?php the_archive_title(); ?>
+            </h1>
+        </header>
+    </div>
+
+    <section class="site-section posts-loop">
+        <div class="container">
+            
+            <?php if ( have_posts() ) : ?>
+                <ul class="posts loop">
+                    <?php
+                        while( have_posts() ) {
+                            the_post();
+
+                            shiftr_get_template( 'content-post.php' );
+                        }
+
+                        wp_reset_postdata();
+                    ?>
+                </ul>
+
+                <?php shiftr_pagination(); ?>
+            <?php endif; ?>
+
+        </div>
+    </section>
+</main>
+    
+
+<?php get_footer(); ?>
