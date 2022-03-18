16d5354eaacecef74707604b8a5ae20bef766fa2
Jackson Lewis <hello@jacksonlewis.co.uk>
2022-03-18 20:45:34 +0000

    BEM naming fixes

--- parts/content-post.php
+++ parts/content-post.php
@@ -5,10 +5,10 @@
 ?>
 <li class="post post-<?php echo get_the_ID(); ?>">
     <a href="<?php the_permalink(); ?>">
-        <div class="post-thumbnail">
+        <div class="post__thumbnail">
             <?php shiftr_featured_image( get_the_ID(), 'medium' ); ?>
         </div>
-        <div class="post-summary">
+        <div class="post__summary">
             <h3 id="post-<?php echo get_the_ID(); ?>"><?php the_title(); ?></h3>
             <time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time( 'd F Y' ); ?></time>
         </div>
