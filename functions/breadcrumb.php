77ceb74463007b7b9110419e3af59ecb1c25e647
Jackson Lewis <hello@jacksonlewis.co.uk>
2022-03-18 18:54:51 +0000

    Breadcrumb support for Rank Math

--- functions/breadcrumb.php
+++ functions/breadcrumb.php
@@ -1,10 +1,38 @@
 <?php
 
-function shiftr_breadcrumb() {
+/**
+ * Global wrapper function to breadcrumbs from any provider.
+ * 
+ * @param bool $echo Should function echo or return output
+ * @return string
+ */
+function shiftr_breadcrumb( bool $echo = true ) {
+    $found_provider = false;
 
-    if ( function_exists('yoast_breadcrumb') ) {
-        echo '<div class="site-breadcrumb"><div class="container">';
+    ob_start();
+    echo '<div class="site-breadcrumb"><div class="container">';
+
+    // Yoast
+    if ( function_exists( 'yoast_breadcrumb' ) ) {
         yoast_breadcrumb();
-        echo '</div></div>';
+
+        $found_provider = true;
     }
+
+    // Rank Math
+    if ( function_exists('rank_math_the_breadcrumbs') ) {
+        rank_math_the_breadcrumbs();
+
+        $found_provider = true;
+    }
+
+    echo '</div></div>';
+
+    $output = ob_get_clean();
+
+    if ( $found_provider && $echo ) {
+        echo $output;
+    }
+
+    return $output;
 }
