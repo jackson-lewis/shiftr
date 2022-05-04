cbd1c6bf823c056bb6baa9f95754cb6a24acdd31
Alfie Mills <63820393+Alfie-Mills@users.noreply.github.com>
2022-05-04 12:18:00 +0100

    Revert "Update admin.php"
    This reverts commit 61edcd5171e34826fa4cea9d536955a97d1e346f.

--- functions/admin.php
+++ functions/admin.php
@@ -97,8 +97,3 @@ function shiftr_set_display_post_states( $post_states, $post ) {
     return $post_states;
 }
 add_filter( 'display_post_states', 'shiftr_set_display_post_states', 2, 10 );
-
-/**
- * Add "Developer" user role
- */
-add_role( 'dev', 'Developer', get_role( 'administrator' )->capabilities );
