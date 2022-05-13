6e6eb855462df32eef80963af35ec4068929788e
Jackson Lewis <hello@jacksonlewis.co.uk>
2022-05-13 18:46:13 +0100

    Hello Tabs!

--- src/scripts/components/tabs.js
+++ src/scripts/components/tabs.js
@@ -14,7 +14,10 @@ export default class Tabs extends ShiftrComponent {
     }
 
     defaultSettings() {
-        return {}
+        return {
+            tabList: '.shiftr-tabs__tab',
+            tabPanel: '.shiftr-tabs__panel'
+        }
     }
 
     /**
@@ -23,15 +26,73 @@ export default class Tabs extends ShiftrComponent {
     constructor( ...args ) {
         super( ...args )
 
-        this.items = this.target.querySelectorAll( this.settings.itemsTarget )
+        this.tabs = this.target.querySelectorAll( this.settings.tabList )
+        this.panels = this.target.querySelectorAll( this.settings.tabPanel )
     }
 
     /**
      * Initiate the component
      */
-    init() {}
+    init() {
+        this.target.setAttribute( 'role', 'tablist' )
 
+        this.tabs.forEach( ( tab, index ) => {
+            const tabID = `${ this.componentID }-tab_${ index }`,
+                  panelID = `${ this.componentID }-panel_${ index }`
 
+            tab.setAttribute( 'id', tabID )
+            tab.setAttribute( 'role', 'tab' )
+            tab.setAttribute( 'aria-selected', false )
+            tab.setAttribute( 'aria-controls', panelID )
+            tab.setAttribute( 'tabindex', -1 )
 
+            tab.addEventListener( 'click', e => {
+                e.preventDefault()
+
+                this.activateTab( index )
+            })
+        })
+
+        this.panels.forEach( ( panel, index ) => {
+            const tabID = `${ this.componentID }-tab_${ index }`,
+                  panelID = `${ this.componentID }-panel_${ index }`
+
+            panel.setAttribute( 'id', panelID )
+            panel.setAttribute( 'role', 'tabpanel' )
+            panel.setAttribute( 'aria-labelledby', tabID )
+            panel.setAttribute( 'tabindex', 0 )
+        })
+
+        this.activateTab( 0 )
+    }
+
+    activateTab( index ) {
+        this.deactivateCurrentTab()
+
+        const tab = this.target.querySelector( `#${ this.componentID }-tab_${ index }` ),
+              panel = this.target.querySelector( `#${ this.componentID }-panel_${ index }` )
+
+        tab.removeAttribute( 'tabindex' )
+        tab.setAttribute( 'aria-selected', true )
+        tab.classList.add( 'is-visible' )
+
+        panel.classList.add( 'is-visible' )
+    }
+
+    deactivateCurrentTab() {
+        const currentTab = this.target.querySelector( '.shiftr-tabs__tab[aria-selected="true"]' )
+
+        if ( currentTab ) {
+            currentTab.classList.remove( 'is-visible' )
+            currentTab.setAttribute( 'tabindex', -1 )
+            currentTab.setAttribute( 'aria-selected', false )
+
+            const currentPanel = this.target.querySelector( `#${ currentTab.getAttribute( 'aria-controls' ) }` )
+
+            if ( currentPanel ) {
+                currentPanel.classList.remove( 'is-visible' )
+            }
+        }
+    }
 }
  
